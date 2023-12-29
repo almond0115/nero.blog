476ae45d7897277570807fd02a46cecf6258075a
JongHyunJung <jjh19960115@gmail.com>
2023-12-30 02:55:18 +0900

    update from gatsby-starter-hoodie

---
+++ my-nero-blog/blog-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: "nero.coding",
+  description: "Hello :) I'm Nero!",
+  author: "Nero",
+  siteUrl: "https://devnero.com/",
+  links: {
+    github: "https://github.com/almond0115",
+    linkedIn: "https://www.linkedin.com/in/jonghyunjung",
+    // facebook: "https://www.facebook.com",
+    // twitter: "https://twitter.com",
+    // x: "https://x.com",
+    instagram: "https://www.instagram.com/viaunixue/",
+    // medium: "https://medium.com",
+    // blogger: "https://blogger.com",
+    email: "mailto:jjh3543@naver.com",
+    resume: "https://nero.blog",
+    link: "https://nero.blog",
+  },
+  utterances: {
+    repo: "almond0115/nero.blog",
+    type: "pathname",
+  },
+}
